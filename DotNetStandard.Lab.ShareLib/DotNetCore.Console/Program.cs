4b6118068abb8045d9d4f96bfc00b8fee9ed89b2
Oscar van Tol <o.van.tol@betabit.nl>
2017-05-19 23:30:28 +0200

    Added .net standard console test apps

---
+++ DotNetStandard.Lab.ShareLib/DotNetCore.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using DotNetStandard.Lab.ShareLib;
+using System;
+
+namespace DotNetCore.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var world = new World();
+            System.Console.WriteLine(world.GetGreeting());
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ DotNetStandard.Lab.ShareLib/FullFramework.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using DotNetStandard.Lab.ShareLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullFramework.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var world = new World();
+            System.Console.WriteLine(world.GetGreeting());
+            System.Console.ReadKey();
+        }
+    }
+}

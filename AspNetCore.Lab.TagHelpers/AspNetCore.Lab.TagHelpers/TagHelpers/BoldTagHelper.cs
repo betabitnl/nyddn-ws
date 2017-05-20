41b2e686d5bd047af7d589d1e5283905b8e7c3db
Rick van den Bosch <rickvdbosch@outlook.com>
2017-05-20 16:37:28 +0200

    Added TagHelper lab solution

---
+++ AspNetCore.Lab.TagHelpers/AspNetCore.Lab.TagHelpers/TagHelpers/BoldTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+// TagHelper properties can be passed via attributes, PascalCasing is translated 
+// into kebab-casing. This means MailTo translates into mail-to.
+namespace AspNetCore.Lab.TagHelpers.TagHelpers
+{
+    [HtmlTargetElement("bold")]
+    [HtmlTargetElement(Attributes = "bold")]
+    // Above attributes are combined with an OR, below would be combined with an AND.
+    // [HtmlTargetElement("bold", Attributes = "bold")]
+    public class BoldTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll("bold");
+            output.PreContent.SetHtmlContent("<strong>");
+            output.PostContent.SetHtmlContent("</strong>");
+        }
+    }
+}
---
+++ AspNetCore.Lab.TagHelpers/AspNetCore.Lab.TagHelpers/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNetCore.Lab.TagHelpers.TagHelpers
+{
+    // TagHelpers are bound to elements on the name. This helper binds to the email tag. The suffix
+    // TagHelper is not mandatory, but it does make things clear to add it.
+    public class EmailTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            var cntnt = await output.GetChildContentAsync();
+            output.Attributes.SetAttribute("href", $"mailto:{cntnt.GetContent()}@betabit.nl");
+        }
+    }
+}
